2b4892cdf72ee8902c5e3ebdc5a7692e45282a42
aeggum <aeggum12@gmail.com>
2016-03-10 08:17:18 -0600

    Lesson 5 - learned about stateless functional components, more routing and a pattern for separating business and presentation logic

---
+++ app/components/Prompt.js
@@ -0,0 +1,42 @@
+var React = require('react');
+var PropTypes = React.PropTypes;
+var transparentBg = require('../styles').transparentBg;
+
+// Functional stateless components
+// Can be used if we're just building a UI and using propTypes.  Don't need React.createClass({ ...
+function Prompt(props) {
+    return (
+        <div className="jumbotron col-sm-6 col-sm-offset-3 text-center" style={transparentBg}>
+            <h1>{props.header}</h1>
+            <div className="col-sm-12">
+                <form onSubmit={props.onSubmitUser}>
+                    <div className="form-group">
+                        <input
+                            className="form-control"
+                            placeholder="Github Username"
+                            onChange={props.onUpdateUser}
+                            value={props.username}
+                            type="text"/>
+                    </div>
+                    <div className="form-group col-sm-4 col-sm-offset-4">
+                        <button
+                            className="btn btn-block btn-success"
+                            type="submit">
+                            Continue
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+Prompt.propTypes = {
+    header: PropTypes.string.isRequired,
+    onSubmitUser: PropTypes.func.isRequired,
+    onUpdateUser: PropTypes.func.isRequired,
+    username: PropTypes.string.isRequired
+};
+
+// TODO: Learn / read / understand how module.exports works
+module.exports = Prompt;
