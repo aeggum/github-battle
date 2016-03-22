bf471a7c27372502437a7f21a07bf741f004bc77
aeggum <aeggum12@gmail.com>
2016-03-21 19:28:52 -0500

    Learned about reduce in javascript, built /results route

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -5,10 +5,6 @@ var Link = require('react-router').Link;
 var UserDetails = require('./UserDetails');
 var UserDetailsWrapper = require('./UserDetailsWrapper');
 
-function puke(object) {
-    return <pre>{JSON.stringify(object, null, ' ')}</pre>
-}
-
 function ConfirmBattle(props) {
     return props.isLoading === true ? <p> LOADING! </p> :
         <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
